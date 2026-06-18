494935467294374c35e00d3c20ccffd66a95691d
Ivan Yong Pak Theng <ivanyong07@gmail.com>
2026-06-18 20:48:43 +0800

    Complete all load information function

--- src/Test/TestLogic.java
+++ src/Test/TestLogic.java
@@ -9,9 +9,14 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.util.ArrayList;
-import model.Admin;
-import model.Receptionist;
+
 import model.User;
+import model.Admin;
+import model.Counselor;
+import model.Receptionist;
+import model.Student;
+
+
 
 public class TestLogic {
 
@@ -24,11 +29,15 @@ public class TestLogic {
     public static ArrayList<User> userList = new ArrayList<>();
     public static ArrayList<Admin> adminList = new ArrayList<>();
     public static ArrayList<Receptionist> receptionistList = new ArrayList<>();
+    public static ArrayList<Counselor> counselorList = new ArrayList<>();
+    public static ArrayList<Student> studentList = new ArrayList<>();
     
     public static void LoadInformation(String userID){
+        boolean found = false;
         try (BufferedReader readerUser = new BufferedReader(new FileReader(fileUser))){
             String userLine;
-            System.out.println("Receptionist ID found userID match userData");
+            
+            
             // add condition for searching the correct file based on the userID
             while ((userLine = readerUser.readLine()) != null){
                 String[] userData = userLine.split("\\|"); // first split having space
@@ -36,10 +45,9 @@ public class TestLogic {
                 for (int i = 0; i < userData.length; i++){
                     userData[i] = userData[i].trim();
                 } // use for loop for trim()
-                
+
                 // Admin Load Information
                 if (userID.startsWith("ADM") && userData[0].equals(userID)){
-                    System.out.println("Current Role account found...");
                     try (BufferedReader readerAdm = new BufferedReader(new FileReader(fileAdmin));){
                         String admLine;
                         while ((admLine = readerAdm.readLine()) != null){
@@ -48,35 +56,38 @@ public class TestLogic {
                                 admData[i] = admData[i].trim();
                             }
                             if (userData[0].equals(admData[0])){
+                                
+                                found = true;
                                 System.out.println("User ID match Role ID: " + userData[0]);
                                 System.out.println("ID Matching...");
+                                
                                 Admin admin = new Admin(userData[0], userData[1], userData[2], userData[3], 
                                         userData[4], userData[5], userData[6], admData[7], admData[8]);
                                 adminList.add(admin);
+                                System.out.println("Done");
+                                break;
                                 
-                                for (Admin m: adminList){
-                                    System.out.println("Gather information successfull...");
-                                    System.out.println(m);
-                                }
-                            } else {
-                                System.out.println("Admin ID not found...Please register new account for the admin...");
-                            }
+                                // check value
+//                                for (Admin m: adminList){
+//                                    System.out.println("Gather information successfull...");
+//                                    System.out.println(m);
+//                              }
+                            } 
                         }
                     }
                     catch (FileNotFoundException e){
                         System.out.println("Admin.txt not Found");
-                    }
-                    
-                    catch (IOException e){
+                    } catch (IOException e){
                         System.out.println("Error: " + e);
                     }
                 }
+                
+                
                 // Reptionist Load Information 
                 if (userID.startsWith("REP") && userData[0].equals(userID)){
-                    System.out.println("Current Role account found...");
+
                     try(BufferedReader repReader = new BufferedReader(new FileReader(fileReceptionist))){
                         String repLine;
-                        System.out.println("Receptionist ID found userID match userData");
                         
                         while ((repLine = repReader.readLine()) != null){
                             String[] repData = repLine.split("\\|");
@@ -84,20 +95,21 @@ public class TestLogic {
                             for (int i = 0; i < repData.length; i++){
                                 repData[i] = repData[i].trim();
                             }
-                            
                             if (userData[0].equals(repData[0])){
+                                found = true;
                                 System.out.println("User ID match Receptionist ID: " + userData[0]);
                                 System.out.println("ID Matching");
                                 Receptionist receptionist = new Receptionist(userData[0], userData[1], userData[2], userData[3], 
                                         userData[4], userData[5], userData[6], repData[7], repData[8]); 
                                 receptionistList.add(receptionist);
+                                System.out.println("Done");
+                                break;
                                 
-                                for (Receptionist r : receptionistList){
-                                    System.out.println("Gathering information...");
-                                    System.out.println("Reptionist: " + r);
-                                }
-                            } else {
-                                System.out.println("Reptionist ID not found please register the new account for reptionist");
+                                // check value
+//                                for (Receptionist r : receptionistList){
+//                                    System.out.println("Gathering information...");
+//                                    System.out.println("Reptionist: " + r);
+//                                }
                             }
                         }
                     } catch (FileNotFoundException e){
@@ -107,18 +119,68 @@ public class TestLogic {
                     }
                 }
                 
+                
+                // Counselor Load Information
                 if (userID.startsWith("CSL") && userData[0].equals(userID)){
-                    System.out.println("Current Role account found...");
                     try (BufferedReader rslReader = new BufferedReader(new FileReader(fileCounselor))){
                         String rslLine;
-                        System.out.println("Counlesor Id found userID match UserData");
-                        
                     while ((rslLine = rslReader.readLine()) !=null){
-                        String[] rslData = rslLine.split("\\|")
-                    }
-                        
+                        String[] cslData = rslLine.split("\\|");
+                        // debug
+//                        System.out.println("cslData[0] = '" + cslData[0].trim() + "'");
+//                        System.out.println("userData[0] = '" + userData[0] + "'");
+                            for (int i = 0; i < cslData.length; i++){
+                                cslData[i] = cslData[i].trim();
+                            }
+                            if(userData[0].equals(cslData[0])) {
+                                found = true;
+                                System.out.println("User ID match Counselor ID: " + userData[0]);
+                                System.out.println("ID Matching...");
+                                Counselor counselor = new Counselor(userData[0], userData[1], userData[2], userData[3], userData[4], 
+                                        userData[5], userData[6], cslData[7], cslData[8]);
+                                counselorList.add(counselor);
+                                System.out.println("Done");
+                                break;
+                                //check value
+    //                            for (Counselor c: counselorList){
+    //                                System.out.println("Counselor: " + c);
+    //                          }
+                            }
+                        } 
                     } catch (FileNotFoundException e){
-                        System.out.println("Counselor not found");
+                        System.out.println("counselor.txt not found");
+                    } catch (IOException e){
+                        System.out.println("Error: " + e);
+                    }
+                }
+                
+                // Student Load Information
+                if(userID.startsWith("STU") && userData[0].equals(userID)){
+                    try (BufferedReader stuReader = new BufferedReader(new FileReader(fileStudent))){
+                        String stuLine;
+                        
+                        while ((stuLine = stuReader.readLine()) != null){
+                            String[] stuData = stuLine.split("\\|");
+                            
+                            for (int i = 0; i < stuData.length; i++){
+                                stuData[i] = stuData[i].trim();
+                            }
+                            
+                            if (userData[0].equals(stuData[0])){
+                                found = true;
+
+                                System.out.println("User ID match Student ID: " + userData[0]);
+                                System.out.println("ID Matching...");
+                                
+                                Student student = new Student(userData[0], userData[1], userData[2], userData[3], userData[4], 
+                                        userData[5], userData[6], stuData[7], stuData[8]);
+                                studentList.add(student);
+                                System.out.println("Done");
+                                break;
+                            }
+                        }
+                    } catch (FileNotFoundException e){
+                        System.out.println("student.txt not found");
                     } catch (IOException e){
                         System.out.println("Error: " + e);
                     }
@@ -129,14 +191,21 @@ public class TestLogic {
         } catch (IOException e){
             System.out.println("File could not read");
         }
+        if (!found){
+            System.out.println("ID not found");
+        }
     }
     
     public static void main(String args[]) {
         LoadInformation("ADM001"); // testing 1 (done)
         System.out.println("");
-        LoadInformation("REP001"); // testing 2 (not done)
+        LoadInformation("REP001"); // testing 2 (done)
         System.out.println("");
-        LoadInformation("CSL001");
+        LoadInformation("CSL001"); // testing 3 (done)
+        System.out.println("");
+        LoadInformation("STU001");
+
+        
         
         // change one line only method
         // if match to the id featch the line that match to the array
